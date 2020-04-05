9418dccd5c51ac57095266f773de6cfd66482c8a
Dipesh Yadav <v-diyadav@expedia.com>
2020-04-05 15:27:50 +0530

    Updates

---
+++ precache-manifest.05490dbce10fc6164f687c42c1a40071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948ff399c51b2ad10de86786478bb954",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9fc5b0d72daf3702d7e2",
+    "url": "/monsters-rolodex/static/css/main.ea5adf3c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fc5b0d72daf3702d7e2",
+    "url": "/monsters-rolodex/static/js/main.03a29841.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
